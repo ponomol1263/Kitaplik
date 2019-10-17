27106507ead485c063643e9f877ebef65c58262d
Selinnn <kaplanselinn@gmail.com>
2019-10-17 05:25:03 +0300

    Update

--- app/src/main/java/com/codeandmania/kitaplik/activities/KitapActivityMain.java
+++ app/src/main/java/com/codeandmania/kitaplik/activities/KitapActivityMain.java
@@ -20,7 +20,7 @@ public class KitapActivityMain extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), FilmAnasayfa.class);
+                Intent intent = new Intent(getApplicationContext(), BookList.class);
 
                 startActivity(intent);
             }
